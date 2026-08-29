988d77c49a45ec0ca36a7d35a321eabb81b10133
Rajsfano Priariyabarisky <rajsfanopriariyabarisky@gmail.com>
2026-08-29 18:10:41 +0700

    Fix HTTPS proxy handling on Railway

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Schema;
@@ -23,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (app()->environment('production')) {
+    URL::forceScheme('https');
+}
         View::composer('*', function ($view) {
 
             $pendingCount = 0;
